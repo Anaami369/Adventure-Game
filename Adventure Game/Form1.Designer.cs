19d80dd2620acce6313432ebbcc0cd0e6532b10e
Anaami Patel <anaamisvm@gmail.com>
2020-04-10 11:51:53 -0400

    hi

---
+++ Adventure Game/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Adventure_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.blueButton = new System.Windows.Forms.Button();
+            this.redButton = new System.Windows.Forms.Button();
+            this.blueLabel = new System.Windows.Forms.Label();
+            this.redLabel = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // blueButton
+            // 
+            this.blueButton.BackColor = System.Drawing.Color.Transparent;
+            this.blueButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.blueButton.Image = global::Adventure_Game.Properties.Resources.blue_button;
+            this.blueButton.Location = new System.Drawing.Point(15, 310);
+            this.blueButton.Name = "blueButton";
+            this.blueButton.Size = new System.Drawing.Size(60, 60);
+            this.blueButton.TabIndex = 1;
+            this.blueButton.UseVisualStyleBackColor = false;
+            this.blueButton.Click += new System.EventHandler(this.blueButton_Click);
+            // 
+            // redButton
+            // 
+            this.redButton.BackColor = System.Drawing.Color.Transparent;
+            this.redButton.BackgroundImage = global::Adventure_Game.Properties.Resources.red_button;
+            this.redButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.redButton.Image = global::Adventure_Game.Properties.Resources.red_button;
+            this.redButton.Location = new System.Drawing.Point(15, 383);
+            this.redButton.Name = "redButton";
+            this.redButton.Size = new System.Drawing.Size(60, 60);
+            this.redButton.TabIndex = 2;
+            this.redButton.UseVisualStyleBackColor = false;
+            this.redButton.Click += new System.EventHandler(this.redButton_Click);
+            // 
+            // blueLabel
+            // 
+            this.blueLabel.BackColor = System.Drawing.Color.Transparent;
+            this.blueLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blueLabel.ForeColor = System.Drawing.Color.White;
+            this.blueLabel.Location = new System.Drawing.Point(81, 310);
+            this.blueLabel.Name = "blueLabel";
+            this.blueLabel.Size = new System.Drawing.Size(707, 50);
+            this.blueLabel.TabIndex = 3;
+            this.blueLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // redLabel
+            // 
+            this.redLabel.BackColor = System.Drawing.Color.Transparent;
+            this.redLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.redLabel.ForeColor = System.Drawing.Color.White;
+            this.redLabel.Location = new System.Drawing.Point(84, 383);
+            this.redLabel.Name = "redLabel";
+            this.redLabel.Size = new System.Drawing.Size(704, 50);
+            this.redLabel.TabIndex = 4;
+            this.redLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.Transparent;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(12, 9);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(371, 298);
+            this.outputLabel.TabIndex = 5;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Image = global::Adventure_Game.Properties.Resources.river_bank;
+            this.pictureBox.Location = new System.Drawing.Point(396, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(392, 295);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 6;
+            this.pictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(797, 450);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.redLabel);
+            this.Controls.Add(this.blueLabel);
+            this.Controls.Add(this.redButton);
+            this.Controls.Add(this.blueButton);
+            this.Controls.Add(this.pictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button blueButton;
+        private System.Windows.Forms.Button redButton;
+        private System.Windows.Forms.Label blueLabel;
+        private System.Windows.Forms.Label redLabel;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ Adventure Game/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Adventure_Game
+{
+    public partial class Form1 : Form
+    {
+
+        int life;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            outputLabel.Text = "You wake up beside a river on a river bank.You try to \n turn on your phone, it's dead, but you remember \n charging it when you went to sleep.";
+
+            redLabel.Text = "Look for a clue in the shack";
+            blueLabel.Text = "Look for a clue in the forest";
+
+            pictureBox.Image = Properties.Resources.river_bank;
+        }
+
+        //private bool blueButtonState;
+        //private bool redButtonState;
+
+        private void blueButton_Click(object sender, EventArgs e)
+        {
+            // int blue1 = 1;
+            // int red1 = 1;
+
+            {
+                Button btnSender = (Button)sender;
+
+                if (btnSender == redButton)
+                {
+                    outputLabel.Text = "You find Arachnid Ned (king of Spider land), you have 20% chance of survival";
+                    pictureBox.Image = Properties.Resources.arachnid_ned;
+
+                    Random randGen = new Random();
+                    life = randGen.Next(1, 6);
+
+                    switch (life)
+                    {
+                        case 1:
+                            pictureBox.Image = Properties.Resources.lost;
+
+                            break;
+                        case 2:
+                            pictureBox.Image = Properties.Resources.lost;
+
+                            break;
+                        case 3:
+                            pictureBox.Image = Properties.Resources.lost;
+
+                            break;
+                        case 4:
+                            pictureBox.Image = Properties.Resources.lost;
+
+                            break;
+                        case 5:
+                            outputLabel.Text = "He seems to be hungry and all the fleas on his back seemed to be aggravating him.";
+                            redLabel.Text = "run";
+                            blueLabel.Text = "walk";
+
+                            pictureBox.Image = Properties.Resources.arachnid_ned;
+
+                            break;
+                    }
+                }
+
+                else if (btnSender == blueButton)
+                {
+                    outputLabel.Text = "You find two paths";
+
+                    redLabel.Text = "Go right";
+                    blueLabel.Text = "Go left";
+
+                    pictureBox.Image = Properties.Resources.two_paths;
+                }
+            }
+        }
+
+        /* blueButtonState = true;
+        int blue1 = 1;
+        int red1 = 1;
+
+        outputLabel.Text = "You find two paths";
+
+        redLabel.Text = "Go right";
+        blueLabel.Text = "Go left";
+
+        pictureBox.Image = Properties.Resources.two_paths;
+
+        blue1 += 2;
+
+        if (blue1 == 1)
+        {
+            redButtonState = true;
+        }
+
+        if (redButtonState == true & red1 == 1)
+        {
+            outputLabel.Text = "You find Arachnid Ned (king of Spider land)";
+
+            Random randGen = new Random();
+            life = randGen.Next(1, 3);
+
+            switch (life)
+            {
+                case 1:
+
+                    break;
+            }
+
+            redLabel.Text = "You drink";
+            blueLabel.Text = "Go across";
+
+            pictureBox.Image = Properties.Resources.lake;
+
+            red1 += 2;
+        }
+
+        if (blueButtonState == true & blue1 == 3)
+        {
+            outputLabel.Text = "You encounter a lake, You are thirsty";
+
+            redLabel.Text = "You drink";
+            blueLabel.Text = "Go across";
+
+            pictureBox.Image = Properties.Resources.lake;
+
+            blue1 += 2;
+        }
+    }
+    */
+        private void redButton_Click(object sender, EventArgs e)
+        {
+            Button btnSender = (Button)sender;
+
+            if (btnSender == redButton)
+            {
+                outputLabel.Text = "You find Arachnid Ned (king of Spider land), you have 20% chance of survival";
+                pictureBox.Image = Properties.Resources.arachnid_ned;
+
+                Random randGen = new Random();
+                life = randGen.Next(1, 6);
+
+                switch (life)
+                {
+                    case 1:
+                        pictureBox.Image = Properties.Resources.lost;
+                        return;
+                    case 2:
+                        pictureBox.Image = Properties.Resources.lost;
+                        return;
+                    case 3:
+                        pictureBox.Image = Properties.Resources.lost;
+                        return;
+                    case 4:
+                        pictureBox.Image = Properties.Resources.lost;
+                        return;
+                    case 5:
+                        outputLabel.Text = "He seems to be hungry and all the fleas on his back seemed to be aggravating him.";
+                        redLabel.Text = "run";
+                        blueLabel.Text = "walk";
+
+                        pictureBox.Image = Properties.Resources.arachnid_ned;
+
+                        break;
+                }
+            }
+        }
+    }
+}
